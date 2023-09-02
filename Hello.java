7cfd41e9b9b27c1f2f59cc7b715d362943d50acd
stud-mauu <maureen.sebastian@neu.edu.ph>
2023-09-03 02:03:05 +0800

    add some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering 1!");
     System.out.println("Student name: SEBASTIAN, Maureen-Mae");
+    System.out.println("That is why developers are highly-paid!");
+    System.out.println("Complicated world of SoftDev that is why it pays much!");
   }
 }
